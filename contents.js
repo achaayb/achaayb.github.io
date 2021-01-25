4ec1b90f0beb14059a52b270cc8ccb1e0854f434
okay aka PseudoROX <z361s2wuga@protonmail.com>
2021-01-25 02:25:05 +0000

    fixed services

--- contents.js
+++ contents.js
@@ -7,11 +7,10 @@ const whoamiContent = `
             <div class="whoami__description">
                 <div class="whoami__description__actual">
                     <h1>Ali Chaayb</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Vitae at voluptatibus corrupti voluptate autem sint nam 
-                    veniam libero nisi rem fugit quasi, deserunt, corporis dolor, 
-                    nihil ipsum qui tenetur consequatur dignissimos officiis 
-                    repellendus illum architecto vel inventore! Enim, rerum?</p>
+                    <p>Cybersecurity researcher, Developer, And developement team leader
+                    passionate about programming give me money give me money give me money
+                    give me money give me money give me money give me money give me money
+                    </p>
                 </div>
                 <div class="whoami__description__footer">
                     <p>Interested?</p>
@@ -27,25 +26,51 @@ const projectsContent = `
     <h1>This is projects</h1>
 `
 const servicesContent = `
-    <div class="content">
-        <ul class="services__wrapper">
-            ${
-                services.map(service => {
-                    return (`
-                    <li class="cards_item">
-                        <div class="card">
-                            <div class="card_image"><img src="https://picsum.photos/500/300/?image=10"></div>
-                            <div class="card_content">
-                                <h2 class="card_title">Card Grid Layout</h2>
-                                <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-                                <button class="btn card_btn">Read More</button>
-                            </div>
-                        </div>
-                    </li>`)
-                })
-            }
-        </ul>
+
+<div class="visi">
+<ul class="cards">
+  <li class="cards__item">
+    <div class="card">
+      <div class="card__image card__image--fence"></div>
+      <div class="card__content">
+        <div class="card__title">Flex</div>
+        <p class="card__text">This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
+        <button class="btn btn--block card__btn">Button</button>
+      </div>
     </div>
+  </li>
+  <li class="cards__item">
+    <div class="card">
+      <div class="card__image card__image--river"></div>
+      <div class="card__content">
+        <div class="card__title">Flex Grow</div>
+        <p class="card__text">This defines the ability for a flex item to grow if necessary. It accepts a unitless value that serves as a proportion. It dictates what amount of the available space inside the flex container the item should take up.</p>
+        <button class="btn btn--block card__btn">Button</button>
+      </div>
+    </div>
+  </li>
+  <li class="cards__item">
+    <div class="card">
+      <div class="card__image card__image--record"></div>
+      <div class="card__content">
+        <div class="card__title">Flex Shrink</div>
+        <p class="card__text">This defines the ability for a flex item to shrink if necessary. Negative numbers are invalid.</p>
+        <button class="btn btn--block card__btn">Button</button>
+      </div>
+    </div>
+  </li>
+  <li class="cards__item">
+    <div class="card">
+      <div class="card__image card__image--flowers"></div>
+      <div class="card__content">
+        <div class="card__title">Flex Basis</div>
+        <p class="card__text">This defines the default size of an element before the remaining space is distributed. It can be a length (e.g. 20%, 5rem, etc.) or a keyword. The auto keyword means "look at my width or height property."</p>
+        <button class="btn btn--block card__btn">Button</button>
+      </div>
+    </div>
+  </li>
+</ul>
+</div>
 `
 const platformsContent = `
     <h1>This is platforms</h1>
